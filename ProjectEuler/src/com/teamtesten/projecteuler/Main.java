47530cb3750eea390af7e76fc22e4c6143efa1dd
Mike Testen <mrtesten@gmail.com>
2013-02-27 17:59:18 -0500

    Extracted the main function into a new class.

---
+++ ProjectEuler/src/com/teamtesten/projecteuler/Main.java
@@ -0,0 +1,40 @@
+package com.teamtesten.projecteuler;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		Solver solver = null;
+
+		if(args.length > 0)
+		{
+			solver = new Solver(args[0]);
+		}
+		else
+		{
+			try
+			{
+				BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+
+				System.out.print("Which problem would you like to solve? ");
+				String input = br.readLine();
+				
+				solver = new Solver(input);
+			}
+			catch (IOException e)
+			{
+				System.out.println("Invalid input (" + e.getMessage() + ").");
+			}
+		}
+
+		if(solver != null)
+		{
+			solver.solve();
+			solver.printResults();
+		}
+	}
+}
--- ProjectEuler/src/com/teamtesten/projecteuler/Solver.java
+++ ProjectEuler/src/com/teamtesten/projecteuler/Solver.java
@@ -1,8 +1,5 @@
 package com.teamtesten.projecteuler;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.math.BigInteger;
 
 import com.teamtesten.projecteuler.utilities.ProblemFactory;
@@ -52,38 +49,6 @@ public class Solver
 			System.out.println("Problem " + problem.getProblemNumber() + ": " + problem.getProblemText());
 			System.out.println("Result: " + result.toString());
 			System.out.println("Execution Time: " + evaluationTime + "ms.");
-		}
-	}
-
-	public static void main(String[] args)
-	{
-		Solver solver = null;
-
-		if(args.length > 0)
-		{
-			solver = new Solver(args[0]);
-		}
-		else
-		{
-			try
-			{
-				BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
-
-				System.out.print("Which problem would you like to solve? ");
-				String input = br.readLine();
-				
-				solver = new Solver(input);
-			}
-			catch (IOException e)
-			{
-				System.out.println("Invalid input (" + e.getMessage() + ").");
-			}
-		}
-
-		if(solver != null)
-		{
-			solver.solve();
-			solver.printResults();
 		}
 	}
 }
